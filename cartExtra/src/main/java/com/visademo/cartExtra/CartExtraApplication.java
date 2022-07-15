beabfedf7c96a05b57feda30fbf38d4b4bedd1b9
Firdaus Razak <razakfirdaus95@gmail.com>
2022-07-15 13:39:51 +0800

    first com

---
+++ cartExtra/src/main/java/com/visademo/cartExtra/CartExtraApplication.java
@@ -0,0 +1,13 @@
+package com.visademo.cartExtra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartExtraApplication.class, args);
+	}
+
+}
---
+++ cartExtra/src/test/java/com/visademo/cartExtra/CartExtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visademo.cartExtra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartExtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
